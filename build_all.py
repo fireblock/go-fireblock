f1a10c8b181702fb38234f1ab82158487a5a6904
Laurent Mallet <laurent.mallet@gmail.com>
2018-02-05 12:39:25 +0100

    rename fbk into fio

--- build_all.py
+++ build_all.py
@@ -6,7 +6,7 @@ import shutil
 from subprocess import call
 
 PLATFORMS = ["windows/amd64", "windows/386", "darwin/amd64", "linux/amd64", "linux/386"]
-VERSION = "0.1.1"
+VERSION = "0.1.2"
 
 if __name__ == "__main__":
     if not os.path.exists("build"):
@@ -22,9 +22,9 @@ if __name__ == "__main__":
         els = p.split('/')
         eos = els[0]
         arch = els[1]
-        archive = "fbk-%s-%s-%s.tar.gz" % (eos, arch, VERSION)
-        cmdBuild = "GOOS=%s GOARCH=%s go build -o build/%s github.com/fireblock/go-fireblock" % (eos, arch, "fbk")
-        cmdArch = "tar zcvf dist/%s build/%s build/LICENSE" % (archive, "fbk")
+        archive = "fio-%s-%s-%s.tar.gz" % (eos, arch, VERSION)
+        cmdBuild = "GOOS=%s GOARCH=%s go build -o build/%s github.com/fireblock/go-fireblock" % (eos, arch, "fio")
+        cmdArch = "tar zcvf dist/%s build/%s build/LICENSE" % (archive, "fio")
         call(cmdBuild, shell=True)
         call(cmdArch, shell=True)
 
