8e130dfbaec26086cda56e297f9e2ac47ec71e6d
Laurent Mallet <laurent.mallet@gmail.com>
2018-02-06 06:42:07 +0100

    fix typo

--- common/common.go
+++ common/common.go
@@ -13,8 +13,9 @@
 //
 // You should have received a copy of the GNU Lesser General Public License
 // along with the go-fireblock library. If not, see <http://www.gnu.org/licenses/>.
-
+//
 // Package common contains various helper functions.
+
 package common
 
 import (
