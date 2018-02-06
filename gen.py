e680960e954b44592babcedcee76f70112045041
Laurent Mallet <laurent.mallet@gmail.com>
2018-02-06 23:01:00 +0100

    update packages

--- gen.py
+++ gen.py
@@ -7,5 +7,6 @@ from subprocess import call
 
 if __name__ == "__main__":
     cmdStore = "abigen --abi abi/store.abi --pkg contracts --type Store --out contracts/store.go"
-    cmdStore = "abigen --abi abi/fireblock.abi --pkg contracts --type Fireblock --out contracts/fireblock.go"
+    cmdFireblock = "abigen --abi abi/fireblock.abi --pkg contracts --type Fireblock --out contracts/fireblock.go"
     call(cmdStore, shell=True)
+    call(cmdFireblock, shell=True)
